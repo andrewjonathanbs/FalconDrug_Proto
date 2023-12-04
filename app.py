bcd8a59874349b2fac2651ac9894af4b4731eedc
Andrew Jonathan Brahms S <109294213+andrewjonathanbs@users.noreply.github.com>
2023-12-04 21:43:40 +0700

    app.py

--- app.py
+++ app.py
@@ -49,7 +49,6 @@ if text:
     state.text_received.append(text)
     for text in state.text_received:
         answer = llm_chain.run(text)
-        answer = translator1.translate(answer)
         obj = gTTS(text=answer, lang=language, slow=False)
         obj.save('trans.mp3')
         audio_file = open('trans.mp3', 'rb')
